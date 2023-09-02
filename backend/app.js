8dc7909d2b65240eae834d429ce51bddda739c51
Senyakkk <senstorage@ya.ru>
2023-09-02 12:09:41 +0300

    gitignor

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./utils/errors/NotFound');
 
-const { PORT = 80, MONGO_DB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, MONGO_DB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
